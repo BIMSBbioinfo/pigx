433e37c18105e98574335c30c6e88e1398fbe584
Franke <vedran.franke@gmail.com>
2017-06-06 18:30:22 +0200

    Extend_Regions: warnings for start < 0

--- Scripts/ChIP/Scripts/Extend_Regions.R
+++ Scripts/ChIP/Scripts/Extend_Regions.R
@@ -11,6 +11,10 @@ Extend_Regions = function(inpath, outpath, extend=NULL){
   setnames(f,c('chr','start','end','name','x','strand'))
   f[strand == '+', end := start[strand == '+'] + as.integer(extend)]
   f[strand == '-', start := end[strand == '-'] - as.integer(extend)]
+  if(any(f$start > 0)){
+      warning('Removing reads with start < 0')
+      f = f[f$start > 0]
+  }
   write.table(f, outpath, row.names=F, col.names=F, quote=F, sep="\t")
 }
 
