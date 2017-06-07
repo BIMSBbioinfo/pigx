905afab2e28321b12234908c13c111608aaa447c
Franke <vedran.franke@gmail.com>
2017-06-07 14:25:31 +0200

    separating ChIP and scRNA pieplines: emoved scRNA folder

--- Scripts/scRNA/FDamm_RunSnake.sh
+++
@@ -1,4 +0,0 @@
-# ---------------------------------------------------------------------------------- #
-SNAKEFILE='/home/vfranke/Projects/FDamm_scRNAseq/Scripts/Snakemake/Snake_Dropseq.py'
-WORKDIR='/data/akalin/Projects/FDamm_scRNAseq'
-snakemake -R --snakefile $SNAKEFILE --directory $WORKDIR --timestamp --jobs 24 --cluster "qsub -V -l h_vmem={params.mem} -pe smp {params.threads} -l h_rt=36:00:00" --rerun-incomplete --latency-wait 30
