e4d050424f8f5c64f7a80d45a4d2b52732143b74
Alexander Gosdschan <alexander.gosdschan@mdc-berlin.de>
2017-06-16 14:11:47 +0200

    add final report to output

--- BSseq_pipeline.py
+++ BSseq_pipeline.py
@@ -91,7 +91,7 @@ OUTPUT_FILES = [
 
                 # ==================  FINAL REPORT =========================
                 # TODO: This needs to be editted once we determine what final reports we want to export!
-		[ expand ( Annot(DIR_annot, config["SAMPLES"][sample]["files"], VERSION, config["SAMPLES"][sample]["SampleID"] ) ) for sample in config["SAMPLES"]  ]
+		            [ expand ( Final(DIR_final, config["SAMPLES"][sampleID]["fastq_name"], VERSION )) for sampleID in config["SAMPLES"]  ]
                 
                ]
 
