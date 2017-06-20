e71a57dabd1a8afbcdfef203fcfb53aa41c293bf
Franke <vedran.franke@gmail.com>
2017-06-20 11:07:04 +0200

    BigWigExtend.R: replaces Extend_Regions.R
    BigWigExtend.R:
    	- bigWig creation ported into R
    	- enables scalling with a given scalling factor

---
+++ Scripts/BigWigExtend.R
@@ -0,0 +1,35 @@
+Extend_Regions = function(inpath, outpath, extend=NULL, scale_index=FALSE, scale_factor=1e6){
+
+    suppressPackageStartupMessages(library(GenomicRanges))
+    suppressPackageStartupMessages(library(data.table))
+    suppressPackageStartupMessages(library(rtracklayer))
+    if(!is.numeric(extend))
+        stop('Extend_Regions: extend parameter needs to be a number')
+
+    if(!file.exists(inpath))
+        stop('Extend_Regions: input file does not exist')
+
+
+    f = fread(inpath)
+    setnames(f,c('chr','start','end','name','x','strand'))
+    f[strand == '+', end := start[strand == '+'] + as.integer(extend)]
+    f[strand == '-', start := end[strand == '-'] - as.integer(extend)]
+    if(any(f$start < 0)){
+        warning('Removing reads with start < 0')
+        f = f[f$start >= 0]
+    }
+
+    g = makeGRangesFromDataFrame(as.data.frame(f))
+    cov = coverage(g)
+
+    if(scale_index == 'yes' || scale_index)
+        cov = round(cov * (scale_factor/nrow(f)),2)
+
+    export.bw(cov, outpath)
+}
+
+Extend_Regions(
+  inpath  = snakemake@input[['file']],
+  outpath = snakemake@output[['file']],
+  extend  = snakemake@params[['extend']],
+  scale_index   = snakemake@params[['scale']])
