ef459cc84f947683ff9961f5c093cc900d3fa6d3
Franke <vedran.franke@gmail.com>
2017-07-11 18:30:56 +0200

    Pipeline works on test cases

--- Snake_Dropseq.py
+++ Snake_Dropseq.py
@@ -97,6 +97,7 @@ GENOME_NAME_PRIMARY = config['annotation']['primary']['genome']['name']
 REFERENCE_NAMES = [GENOME_NAME_PRIMARY]
 
 SAMPLE_NAMES = config['samples'].keys()
+PARAMS = config['params']
 
 # ----------------------------------------------------------------------------- #
 # PATHS
@@ -139,7 +140,8 @@ LINK_GTF_PRIMARY         = os.path.join(PATH_ANNOTATION_PRIMARY, GENOME_NAME_PRI
 # ----------------------------------------------------------------------------- #
 # Combine primary and secondary reference genomes 
 COMBINE_REFERENCE = []
-if not GENOME_NAME_MIX == 'None':
+GENOME_SECONDARY_IND = not GENOME_NAME_MIX == None
+if GENOME_SECONDARY_IND:
     PATH_REFERENCE_MIX = os.path.join(PATH_ANNOTATION_MIX, GENOME_NAME_MIX + '.fasta')
     PATH_GTF_MIX = os.path.join(PATH_ANNOTATION_MIX, GENOME_NAME_MIX + '.gtf')
     COMBINE_REFERENCE = COMBINE_REFERENCE + [PATH_REFERENCE_MIX, PATH_GTF_MIX]
@@ -147,11 +149,11 @@ if not GENOME_NAME_MIX == 'None':
     
 # ----------------------------------------------------------------------------- #
 # REFFLAT and DICT
-REFFLAT = [os.path.join(PATH_ANNOTATION_PRIMARY, GENOME_NAME_PRIMARY + '.refflat')]
+REFFLAT = [os.path.join(PATH_ANNOTATION_PRIMARY, GENOME_NAME_PRIMARY + '.refFlat')]
 DICT    = [os.path.join(PATH_ANNOTATION_PRIMARY, GENOME_NAME_PRIMARY + '.dict')]
 
-if not GENOME_NAME_MIX == 'None':
-    REFFLAT = REFFLAT + [os.path.join(PATH_ANNOTATION_MIX, GENOME_NAME_MIX + '.refflat')]
+if GENOME_SECONDARY_IND:
+    REFFLAT = REFFLAT + [os.path.join(PATH_ANNOTATION_MIX, GENOME_NAME_MIX + '.refFlat')]
     DICT    = DICT    + [os.path.join(PATH_ANNOTATION_MIX, GENOME_NAME_MIX + '.dict')]
 
 # ----------------------------------------------------------------------------- #
@@ -159,24 +161,31 @@ if not GENOME_NAME_MIX == 'None':
 PATH_GTF_PRIMARY_ID = expand(os.path.join(PATH_ANNOTATION_PRIMARY, "{name}" + "gene_id.gtf"), name=REFERENCE_NAMES)
 
 # ----------------------------------------------------------------------------- #
-# Merge fastq files
+# STAR INDEX
+MAKE_STAR_INDEX = expand(os.path.join(PATH_ANNOTATION, '{genome}','STAR_INDEX','done.txt'), genome = REFERENCE_NAMES)
+
+# ----------------------------------------------------------------------------- #
+# MERGE BARODE AND READS FASTQ FILES
 MERGE_FASTQ_TO_BAM = expand(os.path.join(PATH_MAPPED, "{name}", "{name}" + '.fastq.bam'), name=SAMPLE_NAMES)
 
 # ----------------------------------------------------------------------------- #
 # MAPPING
-os.path.join(PATH_MAPPED, "{name}", "{genome}", "star_gene_exon_tagged.bam")
+MAP_scRNA = expand(os.path.join(PATH_MAPPED, "{name}", "{genome}", "star_gene_exon_tagged.bam"), genome = REFERENCE_NAMES, name = SAMPLE_NAMES)
+
+# ----------------------------------------------------------------------------- #
+# UMI matrix
+UMI = expand(os.path.join(PATH_MAPPED, "{name}", "{genome}",'{name}_{genome}_UMI.Matrix.txt'), genome = REFERENCE_NAMES, name = SAMPLE_NAMES)
 
 # ----------------------------------------------------------------------------- #
 RULE_ALL = []
 RULE_ALL = RULE_ALL + [LINK_REFERENCE_PRIMARY, LINK_GTF_PRIMARY]
 
-
 if len(COMBINE_REFERENCE) > 0:
     RULE_ALL = RULE_ALL + COMBINE_REFERENCE
-    
-RULE_ALL = RULE_ALL + DICT
-RULE_ALL = RULE_ALL + REFFLAT
-RULE_ALL = RULE_ALL + MERGE_FASTQ_TO_BAM
+
+RULE_ALL = RULE_ALL + DICT + REFFLAT + MAKE_STAR_INDEX + MERGE_FASTQ_TO_BAM + MAP_scRNA + UMI
+print(RULE_ALL)
+
 
 # FASTQC  = expand(os.path.join(MAPPED_DIR, "{name}", 'FastQC', '{name}_R2_001_fastqc.zip'), name=NAMES)
 # UMI     = expand(os.path.join(MAPPED_DIR, "{name}", "{genome}",'{name}_{genome}_UMI.Matrix.txt'),
@@ -223,52 +232,80 @@ rule link_primary_annotation:
     """
 
 
+
 # ----------------------------------------------------------------------------- #
-rule combine_reference:
-    input:
-        primary   =  LINK_REFERENCE_PRIMARY,
-        secondary =  PATH_REFERENCE_SECONDARY
-    output:
-        outfile = PATH_REFERENCE_MIX
-    params:
-        threads=1,
-        mem='4G',
-        genome_name_primary   = GENOME_NAME_PRIMARY,
-        genome_name_secondary = GENOME_NAME_SECONDARY
-    message:
-        """
-            Combining fasta files:
-                primary   : {input.primary}
-                secondary : {input.secondary}
-                output: {output.outfile}
-        """
-    shell:"""
-        cat {input.primary}   | perl -pe 's|^>|>{params.genome_name_primary}|' > {output.outfile}
-        cat {input.secondary} | perl -pe 's|^>|>{params.genome_name_secondary}|' >> {output.outfile}
+if GENOME_SECONDARY_IND:
+    rule combine_reference:
+        input:
+            primary   =  LINK_REFERENCE_PRIMARY,
+            secondary =  PATH_REFERENCE_SECONDARY
+        output:
+            outfile = PATH_REFERENCE_MIX
+        params:
+            threads=1,
+            mem='4G',
+            genome_name_primary   = GENOME_NAME_PRIMARY,
+            genome_name_secondary = GENOME_NAME_SECONDARY
+        message:
+            """
+                Combining fasta files:
+                    primary   : {input.primary}
+                    secondary : {input.secondary}
+                    output: {output.outfile}
+            """
+        shell:"""
+            cat {input.primary}   | perl -pe 's|^>|>{params.genome_name_primary}|' >     {output.outfile}
+            cat {input.secondary} | perl -pe 's|^>|>{params.genome_name_secondary}|' >> {output.outfile}
     """
 
 # ----------------------------------------------------------------------------- #
-rule combine_gtf:
+# STAR INDEX
+rule make_star_reference:
     input:
-        primary   =  LINK_GTF_PRIMARY,
-        secondary =  PATH_GTF_SECONDARY
+        fasta = os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.fasta'),
+        gtf   = os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.gtf'),
     output:
-        outfile = PATH_GTF_MIX
+        outfile = os.path.join(PATH_ANNOTATION, '{genome}','STAR_INDEX','done.txt')
     params:
-        threads=1,
-        mem='4G',
-        genome_name_primary   = GENOME_NAME_PRIMARY,
-        genome_name_secondary = GENOME_NAME_SECONDARY
-    message:
-        """
-            Combining gtf files:
-                primary   : {input.primary}
-                secondary : {input.secondary}
-                output: {output.outfile}
+        outdir  = os.path.join(PATH_ANNOTATION, '{genome}','STAR_INDEX'),
+        star    = SOFTWARE['star'],
+        threads = 8,
+        mem     = '40G'
+    message:"""
+        Star reference:
+            input:
+                fasta : {input.fasta}
+                gtf   : {input.gtf}
         """
     shell:"""
-        cat {input.primary}   | perl -pe 's|^|{params.genome_name_primary}|' > {output.outfile}
-        cat {input.secondary} | perl -pe 's|^|{params.genome_name_secondary}|' >> {output.outfile}
+        {params.star} --runMode genomeGenerate --genomeDir {params.outdir} --genomeFastaFiles {input.fasta} --runThreadN {params.threads} --sjdbGTFfile {input.gtf} --sjdbOverhang 99
+        touch {output.outfile}
+"""
+
+# ----------------------------------------------------------------------------- #
+# GIVEN PRIMARY AND SECONDARY GTF, COMBINES THEM INTO ONE GTF FILE
+if GENOME_SECONDARY_IND:
+    rule combine_gtf:
+        input:
+            primary   =  LINK_GTF_PRIMARY,
+            secondary =  PATH_GTF_SECONDARY
+        output:
+            outfile = PATH_GTF_MIX
+        params:
+            threads=1,
+            mem='4G',
+            genome_name_primary   = GENOME_NAME_PRIMARY,
+            genome_name_secondary = GENOME_NAME_SECONDARY
+        message:
+            """
+                Combining gtf files:
+                    primary   : {input.primary}
+                    secondary : {input.secondary}
+                    output: {output.outfile}
+            """
+        shell:"""
+            cat {input.primary}   | perl -pe 's|^|{params.genome_name_primary}|' > {output.outfile}
+            cat {input.secondary} | perl -pe 's|^|{params.genome_name_secondary}|' >> {output.outfile}
     """
 
 # ----------------------------------------------------------------------------- #
@@ -282,7 +319,8 @@ rule fasta_dict:
         picard=SOFTWARE['picard'],
         threads=1,
         mem='4G'
-    message:"""
+    message:
+        """
             Fasta dict:
                 input  : {input}
                 output : {output}
@@ -302,7 +340,8 @@ rule change_gtf_id:
     params:
         threads=1,
         mem='4G'
-    message:"""
+    message:
+        """
             Changing GTF id:
                 input  : {input}
                 output : {output}
@@ -317,13 +356,13 @@ rule gtf_to_refflat:
         dict = os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.dict'),
         gtf  = os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.gene_id.gtf')
     output:
-        os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.refflat')
+        os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.refFlat')
     params:
         droptools=SOFTWARE['droptools'],
         threads=1,
         mem='16G'
     message:"""
-            GTF To refflat:
+            GTF To refFlat:
                 input
                     dict : {input.dict}
                     gtf  : {input.gtf}
@@ -362,36 +401,62 @@ rule merge_fastq_to_bam:
     """
 
 
-# # ----------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------------- #
+# Maps reads using Dropseq tools and STAR
 rule map_scRNA:
     input:
-    refflat = rules.gtf_to_refflat.output,
-        dict = rules.fasta_dict.output,
-        infile = rules.fastq_to_bam.output,
-        genome = lambda wc: GENOMES[wc.genome],
-        reference = lambda wc: REFERENCE[wc.genome]
+        refflat   = rules.gtf_to_refflat.output,
+        dict      = rules.fasta_dict.output,
+        index     = rules.make_star_reference.output,
+        infile    = rules.merge_fastq_to_bam.output,
+        reference = os.path.join(PATH_ANNOTATION, '{genome}', '{genome}.fasta'),
     output:
-        outfile = os.path.join(PATH_MAPPED, "{name}", "{genome}", "star_gene_exon_tagged.bam")
+        outfile   = os.path.join(PATH_MAPPED, "{name}", "{genome}","star_gene_exon_tagged.bam")
     params:
-        outdir    = os.path.join(MAPPED_DIR, "{name}", "{genome}"),
+        outdir    = os.path.join(PATH_MAPPED, "{name}", "{genome}"),
+        genome    = os.path.join(PATH_ANNOTATION, '{genome}', 'STAR_INDEX'),
         droptools = SOFTWARE['droptools'],
         star      = SOFTWARE['star'],
         threads   = 8,
         mem       = '8G'
     log:
-        os.path.join(MAPPED_DIR, "{name}", "{genome}","{name}.log")
+       log = os.path.join(PATH_LOG, "{name}.{genome}.STAR.log")
     message: """
             Mapping scRNA:
                 input:
                     dict   : {input.dict}
                     reads  : {input.infile}
-                    genome : {input.genome}
+                    genome : {params.genome}
         """
     shell:"""
-        {params.droptools}/Drop-seq_alignment.sh -g {input.genome} -d {params.droptools} -o {params.outdir} -s {params.star} -r {input.reference} {input.infile}
+        {params.droptools}/Drop-seq_alignment.sh -g {params.genome} -d {params.droptools} -o {params.outdir} -s {params.star} -r {input.reference} {input.infile}
         touch {output.outfile}
     """
 
+
+# ----------------------------------------------------------------------------- #
+# calculates the UMI matrix
+rule get_umi_matrix:
+    input:
+        infile = rules.map_scRNA.output
+    output:
+        os.path.join(PATH_MAPPED, "{name}", "{genome}",'{name}_{genome}_UMI.Matrix.txt')
+    params:
+        outdir            = os.path.join(PATH_MAPPED, "{name}", "{genome}"),
+        outname           = "{name}_{genome}",
+        droptools         = SOFTWARE['droptools'],
+        threads           = 1,
+        mem               = '8G',
+        genes_per_cell    = PARAMS['genes_per_cell'],
+        num_core_barcodes = PARAMS['num_core_barcodes']
+    message: """
+            Count UMI:
+                input:  {input.infile}
+                output: {output}
+        """
+    shell:"""
+        {params.droptools}/DigitalExpression O={output} I={input} SUMMARY={params.outdir}/{params.outname}_Summary.txt MIN_NUM_GENES_PER_CELL={params.genes_per_cell} NUM_CORE_BARCODES={params.num_core_barcodes}
+		"""
 
 # ----------------------------------------------------------------------------- #
 # rule fastqc:
@@ -418,23 +483,7 @@ rule map_scRNA:
 # 
 
 # 
-# # ----------------------------------------------------------------------------- #
-# rule get_umi_matrix:
-# 	input:
-# 		infile = rules.map_dropseq.output
-# 	output:
-# 		os.path.join(MAPPED_DIR, "{name}", "{genome}",'{name}_{genome}_UMI.Matrix.txt')
-# 	params:
-# 		outdir = os.path.join(MAPPED_DIR, "{name}", "{genome}"),
-# 		outname = "{name}_{genome}",
-# 		droptools=DROPTOOLS,
-# 		threads=1,
-# 		mem='8G'
-# 	message:"""
-# 		Count UMI ...
-# 	"""
-# 	shell:"""
-# 		{params.droptools}/DigitalExpression O={output} I={input} SUMMARY={params.outdir}/{params.outname}_Summary.txt MIN_NUM_GENES_PER_CELL=10 NUM_CORE_BARCODES=10
+
 # 	"""
 # 
 # # ----------------------------------------------------------------------------- #
