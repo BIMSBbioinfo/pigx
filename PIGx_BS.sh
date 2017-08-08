d0b35871b980184029f5dea4ea6612acc6ce4496
Ricardo Wurmus <rekado@elephly.net>
2017-08-08 15:06:43 +0200

    Abort on common shell errors.

--- PIGx_BS.sh
+++ PIGx_BS.sh
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+set -u
+set -o pipefail
+
 
 # #===== DEFAULT PATHS ===== #
 tablesheet="test_CElegans/TableSheet_test.csv"
