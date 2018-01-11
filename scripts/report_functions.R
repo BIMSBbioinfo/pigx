ff847a5019d14ae762154973ba0a10da6c17a3cc
alexg9010 <alexgos@zedat.fu-berlin.de>
2018-01-11 12:41:46 +0100

    add a function render2HTML to render report to html

--- scripts/report_functions.R
+++ scripts/report_functions.R
@@ -25,6 +25,56 @@
 # adapted from
 # https://github.com/rstudio/bookdown/blob/master/R/utils.R
 
+## Wrapper function to run a specific Rmd script
+## which does the computation and generates an HTML report with code included
+render2HTML <- function(reportFile,
+                            outFile,
+                            outDir,
+                            finalReportDir,
+                            report.params=NULL,
+                            self.contained=TRUE,
+                            logFile = NULL)
+{
+  
+  output_format = rmarkdown::all_output_formats(reportFile, 'UTF-8')
+  
+  if(!dir.exists(finalReportDir)) dir.create(finalReportDir, recursive = TRUE)
+  
+  if(is.null(report.params)) report.params <- list()
+  
+  ## render single report
+  message("render single report")
+  
+  ## make independent intermediate dirs
+  interDir <- paste0(outDir,"/",outFile,"_tmp")
+  
+  rmarkdown::render(
+    input = reportFile,
+    output_dir = outDir,
+    intermediates_dir = interDir,
+    output_file = outFile,
+    knit_root_dir = outDir,
+    output_format = rmarkdown::html_document(
+      toc = TRUE,
+      toc_float = TRUE,
+      theme = 'lumen',
+      number_sections = FALSE,
+      code_folding = "hide",
+      self_contained = self.contained,
+      includes = list(in_header = "pigx_bsseq_logo.html"),
+      bibliography= "reports.bib"
+    ),
+    params=report.params,
+    quiet = FALSE,
+    clean = TRUE
+  )
+
+  
+  on.exit(unlink(interDir,recursive = TRUE),add = TRUE)
+  
+  
+}
+
 ## modified from https://github.com/rstudio/bookdown/blob/master/R/utils.R#L205-L208
 Rscript_render2 = function(file, ..., log.file=NULL) {
   args = shQuote(c(bookdown:::bookdown_file('scripts', 'render_one.R'), file, ...))
