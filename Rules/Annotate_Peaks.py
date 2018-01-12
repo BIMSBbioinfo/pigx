f251ab8fea0e630ea53fb7663e4630817925aa16
Franke <vedran.franke@gmail.com>
2018-01-12 14:48:12 +0100

    Tested rule for annotating peaks

---
+++ Rules/Annotate_Peaks.py
@@ -0,0 +1,23 @@
+#----------------------------------------------------------------------------- #
+# Annotate Peaks
+rule annotate_peaks:
+        input:
+            annotation = rules.prepare_annoation.outfile.output,
+            peaks      = rules.sort_peak.outfile,
+        output:
+            outfile    = os.path.join(PATH_PEAK,'{name}','{name}.Annotate_Peaks.rds')
+        params:
+            threads     = 1,
+            mem         = '16G',
+            peakname    = '{name}',
+            scriptdir   = SCRIPT_PATH
+        log:
+            log = os.path.join(PATH_LOG, 'prepare_annotation.log')
+        message:"""
+                Running: annotate_peaks:
+                    annot: {input.annotation}
+                    peaks: {input.peaks}
+                    output: {output.outfile}
+            """
+        script:
+            os.path.join(SCRIPT_PATH, 'Annotate_Peaks.R')
