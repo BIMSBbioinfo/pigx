0da9eb2a6b35e368fa8a7a36bb9dcb787a9c2af7
Franke <vedran.franke@gmail.com>
2018-02-09 09:38:56 +0100

    Added signal extraction from annotation and data

--- scripts/Extract_Signal_Annotation.R
+++ scripts/Extract_Signal_Annotation.R
@@ -13,16 +13,18 @@ argv = Parse_Arguments('Extract_Signal_Annotation')
 #' @param scriptdir   -  location of the R scripts and functions
 #'
 #' @return saves RDS object with a list of ScoreMatrix and summarized profiles
+
 Extract_Signal_Annotation = function(
     annotation  = NULL,
     wig         = NULL,
     outfile     = NULL,
-    scriptdir   = NULL
+    scriptdir   = NULL,
+    bin.num     = 10
 ){
 
     # --------------------------------------------------------------- #
     # checks for default arugments
-    deflist = as.list(formals(Annotate_Peaks))
+    deflist = as.list(formals(Extract_Signal_Annotation))
     arglist = as.list(match.call)
     arg.ind = names(deflist) %in% names(arglist)
     if(any(arg.ind))
@@ -31,27 +33,34 @@ Extract_Signal_Annotation = function(
     # --------------------------------------------------------------- #
     suppressPackageStartupMessages(library('genomation'))
     suppressPackageStartupMessages(library('GenomicRanges'))
-    suppressPackageStartupMessages(library('data.table'))
+    suppressPackageStartupMessages(library('stringr'))
+    suppressPackageStartupMessages(library('tibble'))
     source(file.path(scriptdir,'Functions_Helper.R'), local=TRUE)
 
     # --------------------------------------------------------------- #
     message('Annotation ...')
-        annot = readRDS(annotation)
-        annot$genomic_annotation$gene = subset(annot$gtf$gtf, type='gene')
+        annot = readRDS(annotation)$full_annotation
+
 
     message('Extract Profiles ...')
-        nams = names(annot$genomic_annotation)
+        nams = names(annot)
         lsml = lapply(setNames(nams, nams), function(x)
-            ScoreMatrixBin(wig, x[[x]], bin.num = bin.num, strand.aware=TRUE))
+            ScoreMatrixBin(wig, annot[[x]], bin.num = bin.num, strand.aware=TRUE))
 
-    message('Sumarize Profiles ...')
-        profiles = lapply(lsml, function(x){
-            d = data.table(sample = peakname,
-                           signal = rowMeans(x))
-        })
+
+    message('Summarize Profiles ...')
+        sample_name = str_replace(basename(wig),'.bw','')
+        profiles = lapply(names(lsml), function(x){
+              dat = lsml[[x]]
+              dm  = colMeans(dat)
+              d = tibble(genomic_location = x,
+                         sample_name      = sample_name,
+                         position         = seq(dm),
+                         value            = dm)
+          })
 
     lout = list(
-        sml      = lsml,
+        lsml     = lsml,
         profiles = profiles
     )
     saveRDS(lout, outfile)
@@ -64,5 +73,5 @@ Extract_Signal_Annotation(
     wig         = argv$input[['wig']],
     outfile     = argv$output[['outfile']],
     scriptdir   = argv$params[['scriptdir']],
-    peakname    = argv$params[['peakname']]
+    bin.num     = argv$params[['number_of_bins']]
 )
