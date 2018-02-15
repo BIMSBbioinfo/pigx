9d532a55014c3301b6b151c293dcc97960592230
Franke <vedran.franke@gmail.com>
2018-02-15 16:07:02 +0100

    Snake_Dropseq.py: sets default TEMPDIR variable to env TMPDIR

--- Snake_Dropseq.py
+++ Snake_Dropseq.py
@@ -72,6 +72,12 @@ def lookup(column, predicate, fields=[]):
 SAMPLE_NAMES = [line['sample_name'] for line in SAMPLE_SHEET]
 
 # ----------------------------------------------------------------------------- #
+# sets the temporrary directory to default in the working directory if the tempdir does not exist
+if TEMPDIR == None:
+    TEMPDIR = os.environ['TMPDIR']
+
+
+# ----------------------------------------------------------------------------- #
 # RULES
 
 # ----------------------------------------------------------------------------- #
