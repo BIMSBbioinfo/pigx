aa9dfc6a9a4a06e25548728e4f573257541bb741
Franke <vedran.franke@gmail.com>
2018-03-16 14:17:31 +0100

    Peak_Calling.py: finally fixes missing sample params
    fixes #30

--- Rules/Peak_Calling.py
+++ Rules/Peak_Calling.py
@@ -37,9 +37,10 @@ rule macs2:
     """
     run:
         params_macs = params.params_macs
-        if 'macs2' in CUSTOM_PARAMS[params.name].keys():
+        if params.name in set(CUSTOM_PARAMS.keys()):
             if not CUSTOM_PARAMS[params.name] == None:
-                params_macs.update(CUSTOM_PARAMS[params.name]['macs2'])
+                if 'macs2' in CUSTOM_PARAMS[params.name].keys():
+                    params_macs.update(CUSTOM_PARAMS[params.name]['macs2'])
 
         # checks whether the control samples are specified
         samples = ''
