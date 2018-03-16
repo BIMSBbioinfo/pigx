1f06b20d74d355a6e01e5cf413ff71827e1080f1
alexg9010 <alex.gos90@gmail.com>
2018-03-16 11:56:25 +0100

    fix race condition due to moving files

--- Rules/Trimming.py
+++ Rules/Trimming.py
@@ -6,13 +6,13 @@ rule trim_galore_pe:
         r1 = os.path.join(PATH_TRIMMED, "{sample}", "{sample}_R1.fastq.gz"),
         r2 = os.path.join(PATH_TRIMMED, "{sample}", "{sample}_R2.fastq.gz")
     params:
-        tmp1 = lambda wildcards: os.path.join(PATH_TRIMMED, replace_fastq_ext(lookup('SampleName', wildcards.sample, ['Read'])[0],'_val_1.fq.gz')),
-        tmp2 = lambda wildcards: os.path.join(PATH_TRIMMED, replace_fastq_ext(lookup('SampleName', wildcards.sample, ['Read2'])[0],'_val_2.fq.gz')),
+        tmp1 = lambda wildcards: os.path.join(PATH_TRIMMED, wildcards.sample, replace_fastq_ext(lookup('SampleName', wildcards.sample, ['Read'])[0],'_val_1.fq.gz')),
+        tmp2 = lambda wildcards: os.path.join(PATH_TRIMMED, wildcards.sample, replace_fastq_ext(lookup('SampleName', wildcards.sample, ['Read2'])[0],'_val_2.fq.gz')),
         trim_galore = SOFTWARE['trim_galore']['executable']
     log: 
         os.path.join(PATH_LOG, 'trim_galore_{sample}.log')
     shell: 
-        "{params.trim_galore} -o {PATH_TRIMMED} --paired {input.infile[0]} {input.infile[1]} >> {log} 2>&1 && ; mv {params.tmp1} {output.r1} && mv {params.tmp2} {output.r2}"
+        "{params.trim_galore} -o {PATH_TRIMMED}/{wildcards.sample} --paired {input.infile[0]} {input.infile[1]} >> {log} 2>&1 ; mv {params.tmp1} {output.r1} && mv {params.tmp2} {output.r2}"
 
 rule trim_galore_se:
     input: 
@@ -20,9 +20,9 @@ rule trim_galore_se:
     output: 
         read = os.path.join(PATH_TRIMMED,"{sample}", "{sample}_R.fastq.gz")
     params: 
-        tmp = lambda wildcards: os.path.join(PATH_TRIMMED, replace_fastq_ext(lookup('SampleName', wildcards.sample, ['Read'])[0],'_trimmed.fq.gz')),
+        tmp = lambda wildcards: os.path.join(PATH_TRIMMED, wildcards.sample, replace_fastq_ext(lookup('SampleName', wildcards.sample, ['Read'])[0],'_trimmed.fq.gz')),
         trim_galore = SOFTWARE['trim_galore']['executable']
     log: 
         os.path.join(PATH_LOG, 'trim_galore_{sample}.log')
     shell: 
-        "{params.trim_galore} -o {PATH_TRIMMED} {input.infile[0]} >> {log} 2>&1 ; mv {params.tmp} {output.read}"
+        "{params.trim_galore} -o {PATH_TRIMMED}/{wildcards.sample} {input.infile[0]} >> {log} 2>&1 ; mv {params.tmp} {output.read}"
