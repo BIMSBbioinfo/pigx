ea9eff3f05e62bdad2bb6cd0770d196c088390c7
Franke <vedran.franke@gmail.com>
2018-06-13 11:46:01 +0200

    scripts/Sample_Sheet_Class.py: check for empty lines in the sample_sheet.csv
    scripts/Sample_Sheet_Class.py:
    	- check for empty lines when reading sample sheet into class
    
    tests/sample_sheet.csv:
    	- added empty lines

--- scripts/Sample_Sheet_Class.py
+++ scripts/Sample_Sheet_Class.py
@@ -3,20 +3,28 @@ import inspect
 
 ## Experiment Class
 class experiment:
+
+    # ----------------------------------------------------------------------------- #
     def __init__(self, config = [], name="sc_rnaseq"):
         """Return an experiment object whose name is *name*"""
         self.config = config
         self.name = name
         self.SAMPLE_SHEET = []
+    
+    # ----------------------------------------------------------------------------- #
     def init_SAMPLE_SHEET(self, PATH_SAMPLE_SHEET):
         """Load the SAMPLE_SHEET as csv and set the *SAMPLE_SHEET* attribute"""
         self.name = PATH_SAMPLE_SHEET
         with open(PATH_SAMPLE_SHEET, 'r') as fp:
             rows =  [row for row in csv.reader(fp, delimiter=',')]
             header = rows[0]; rows = rows[1:]
-            SAMPLE_SHEET = [dict(zip(header, row)) for row in rows]
+            # removes empty rows from the sample_sheet
+            SAMPLE_SHEET = [dict(zip(header, row)) for row in rows if len(row) > 0]
             self.SAMPLE_SHEET = SAMPLE_SHEET
+            
         self.validate_sheet_init()
+    
+    # ----------------------------------------------------------------------------- #
     def lookup(self, column, predicate, fields=[]):
         """Function for fetching elements from the *SAMPLE_SHEET*"""
         if inspect.isfunction(predicate):
@@ -24,12 +32,18 @@ class experiment:
         else:
             records = [line for line in self.SAMPLE_SHEET if line[column]==predicate]
         return [record[field] for record in records for field in fields]
+
+    # ----------------------------------------------------------------------------- #        
     def list_attr(self, attr):
         """Function for listing values for *atrr*"""
         return [line[attr] for line in self.SAMPLE_SHEET]
+
+    # ----------------------------------------------------------------------------- #        
     def list_rows(self, column, value):
         records = [line for line in self.SAMPLE_SHEET if line[column]==value]
         return records
+ 
+    # ----------------------------------------------------------------------------- #       
     def collapse_technical_replicates(self, PATH_TO_NEW):
         """Function for collapsing technical replicates"""
         sample_names = set(self.list_attr('sample_name'))
@@ -54,10 +68,16 @@ class experiment:
             dict_writer = csv.DictWriter(new_file, columns)
             dict_writer.writeheader()
             dict_writer.writerows(data)
+
+    # ----------------------------------------------------------------------------- #            
     def fetch_reads(self, sample_name):
         return self.to_collapse[sample_name]['reads']
+
+    # ----------------------------------------------------------------------------- #        
     def fetch_barcodes(self, sample_name):
         return self.to_collapse[sample_name]['barcode']
+
+    # ----------------------------------------------------------------------------- #        
     def get_fastq_files(self, s_name, slot):
         """Function to get the FASTQ files associated with a sample"""
         if slot == 'reads':
@@ -72,6 +92,8 @@ class experiment:
                 barcodes.append(os.path.join(PATH_FASTQ, fq_file))
             barcodes.sort()
             return barcodes
+
+    # ----------------------------------------------------------------------------- #            
     def validate_sheet_init(self):
         """Function to validate the sample sheet"""
         sample_sheet = self.SAMPLE_SHEET
