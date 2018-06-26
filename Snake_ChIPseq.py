d1a7a6a108f855279cad9e1b7b7e72101f526bc9
alexg9010 <alex.gos90@gmail.com>
2018-06-26 13:22:56 +0200

    update final report description

--- Snake_ChIPseq.py
+++ Snake_ChIPseq.py
@@ -391,7 +391,7 @@ include: os.path.join(RULES_PATH, 'Summarize_Data_For_Report.py')
 include: os.path.join(RULES_PATH, 'Knit_Report.py')
 
 targets['final-report'] = {
-'description': "Produce a comprehensive report. This is the default target.",
+'description': "Produce a comprehensive report and pan-file quality control report.",
 'files': SUMMARIZED_DATA_FOR_REPORT + REPORT + targets['multiqc']['files']
 }
 
