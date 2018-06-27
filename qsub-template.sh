b8ccbf103bf71532504478bed96e86248569bf38
Franke <vedran.franke@gmail.com>
2018-06-27 15:22:25 +0200

    typo

--- qsub-template.sh
+++ qsub-template.sh
@@ -1,11 +1,12 @@
-#!/home/vfranke/Projects/AAkalin_pigx/pigx_chipseq/pigx_chipseq/run/.guix-profile/bin/bash
+#!/home/vfranke/Projects/AAkalin_pigx/pigx_scrnaseq/run/.guix-profile/bin/bash
 # properties = {properties}
 
 if [ 'yes' = 'yes' ]; then
-    export R_LIBS_SITE="/home/vfranke/Projects/AAkalin_pigx/pigx_chipseq/pigx_chipseq/run/.guix-profile/site-library/"
-    export PYTHONPATH="/home/vfranke/Projects/AAkalin_pigx/pigx_chipseq/pigx_chipseq/run/.guix-profile/lib/python3.6/site-packages"
+    export R_LIBS_SITE="/home/vfranke/Projects/AAkalin_pigx/pigx_scrnaseq/run/.guix-profile/site-library/"
+    export PYTHONPATH="/home/vfranke/Projects/AAkalin_pigx/pigx_scrnaseq/run/.guix-profile/lib/python3.6/site-packages"
 fi
 
 env
+
 
 {exec_job}
