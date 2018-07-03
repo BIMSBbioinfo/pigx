3aa89573e9057fb3136b8f9d96a286e1f088c6ae
Franke <vedran.franke@gmail.com>
2018-07-03 15:13:33 +0200

    Java heap difference is set to 5Gb

--- scripts/Accessory_Functions.py
+++ scripts/Accessory_Functions.py
@@ -2,7 +2,7 @@
 # java_head_difference is the reduction in heap allocation given to the java executible
 # compared to SGE submission: if SGE submission requests 16G, java will be
 # started with (16 - java_heap_difference)G
-def java_tool(java, threads, mem, tempdir, tool_path, tool_name, java_heap_difference=2):
+def java_tool(java, threads, mem, tempdir, tool_path, tool_name, java_heap_difference=5):
 
     # removes 1 g from java memory heap
     mem_size   = int(float(mem[:-1]))
@@ -18,6 +18,7 @@ def java_tool(java, threads, mem, tempdir, tool_path, tool_name, java_heap_diffe
         java,
         '-XX:ParallelGCThreads=' + str(threads),
         '-Xmx'                   + str(mem_reduced),
+        '-Xss'                   + str('16M'),
         '-Djava.io.tmpdir='      + str(tempdir),
         '-jar',
         str(tool_path),
