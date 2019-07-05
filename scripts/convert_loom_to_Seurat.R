1178f40946f7a27f1b90485e797a56120a4d54ce
Franke <vedran.franke@gmail.com>
2019-07-05 15:28:58 +0200

    convert_loom_to_Seurat.R: removed a lot of bugs

--- scripts/convert_loom_to_Seurat.R
+++ scripts/convert_loom_to_Seurat.R
@@ -3,50 +3,12 @@ options = commandArgs(trailingOnly=TRUE)
 source(file.path(options[2],'/Argument_Parser.R'))
 argv = Parse_Arguments('Extract_Read_Statistics')
 
-# ---------------------------------------------------------------------------- #
-read_CellCycle = function(
-    path_cc  = NULL,
-    path_gtf = NULL,
-    organism = NULL
-){
-
-    if(is.null(organism))
-        stop('Please specify an organism')
-
-    if(organism %in% c('mm','hg')){
-        suppressPackageStartupMessages(library(dplyr))
-            gtf = rtracklayer::import.gff(path_gtf)
-            annot = unique(as.data.frame(values(gtf)[,c('gene_id','gene_name')]))
-            cc = read.table(path_cc) %>%
-                mutate(cycle = rep(c('S','G2M'), times=c(43,55))) %>%
-                `colnames<-`(c('gene_name','cycle'))
-
-            if(organims == 'mm'){
-                gname = strsplit(cc$gene_name)
-                gname = lapply(gname, function(x){
-                    s = tolower(x)
-                    s[1] = toupper(s[1])
-                    paste(s, collapse='')
-                })
-                cc$gene_name = unlist(gname)
-            }
-
-            cc = cc %>%
-                left_join(annot, by='gene_name') %>%
-                arrange(cycle)
-    }else{
-        cc = NULL
-    }
-
-    return(cc)
-}
 
 # ---------------------------------------------------------------------------- #
 loomToSeurat = function(
     loom_file       = NULL,
     outfile         = NULL,
     genome_version  = NULL,
-    cycle_genes     = NULL,
     script_path     = NULL
 
 ){
@@ -59,21 +21,19 @@ loomToSeurat = function(
     })
     source(file.path(script_path, 'loom_Functions.R'))
 
-    # cell_cycle = read_CellCycle(
-    #     path_cc  = cycle_genes,
-    #     path_gtf = path_gtf,
-    #     organism = str_replace(genomeBuild,'\\d.+','')
-    # )
-    #
+    
+    
+
     # -------------------------------------------------------------------------#
     loom = loom2sce(loom_file)
 
     meta.data = as.data.frame(SummarizedExperiment::colData(loom))
     colnames(meta.data) = 'CellID'
     raw.data  = Matrix(assays(loom)$counts, nrow=nrow(loom), ncol=ncol(loom))
-    rownames(raw.data) = rowData(loom)$gene_id
-    colnames(raw.data) = meta.data$CellID
-    seu = CreateSeuratObject(raw.data  = raw.data,
+
+    rownames(raw.data) = as.character(rowData(loom)$gene_id)
+    colnames(raw.data) = as.character(meta.data$CellID)
+    seu = CreateSeuratObject(counts    = raw.data, 
                              meta.data = meta.data)
 
     message('Normalize ...')
@@ -83,8 +43,8 @@ loomToSeurat = function(
         # seu = ScaleData(object = seu)
 
     message('Variable genes ...')
-        seu = FindVariableGenes(object = seu, do.plot = FALSE)
-
+        seu = FindVariableFeatures(object = seu, do.plot = FALSE)
+  
     # message('PCA ...')
     #     pcs.compute = min()
     #     seu = RunPCA(seu, do.print=FALSE)
@@ -100,15 +60,15 @@ loomToSeurat = function(
     #                           g2m.genes = g2m.genes,
     #                           set.ident = FALSE)
     # }
-    if(class(rownames(seu@raw.data)) == 'array'){
-        rownames(seu@raw.data) = as.character(rownames(seu@raw.data))
-    }
-
-    if(class(rownames(seu@data)) == 'array'){
-        rownames(seu@data) = as.character(rownames(seu@data))
-    }
-
-    seu@meta.data$CellID = rownames(seu@meta.data)
+    if(class(rownames(seu)) == 'array'){
+        rownames(seu) = as.character(rownames(seu))
+    }  
+        
+    if(class(colnames(seu)) == 'array'){
+        colnames(seu) = as.character(colnames(seu))
+    }  
+        
+    seu@meta.data$CellID = colnames(seu)
     saveRDS(seu, outfile)
 }
 
