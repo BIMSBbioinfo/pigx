864f3f2534aa97293c0feb0b119e34d679cceca4
Franke <vedran.franke@gmail.com>
2019-07-10 20:31:48 +0200

    change_gtf_id.R: suppressPackageStartupMessages

--- scripts/change_gtf_id.R
+++ scripts/change_gtf_id.R
@@ -6,17 +6,20 @@ argv = Parse_Arguments('Extract_Read_Statistics')
 
 # -------------------------------------------------------------------------- #
 change_gtf_id = function(
-  infile  = NULL, 
+  infile  = NULL,
   outfile = NULL
 ){
 
-  library(GenomicRanges)
-  library(rtracklayer)
+  suppressPackageStartupMessages({
+    library(GenomicRanges)
+    library(rtracklayer)
+  })
 
   g = import.gff2(infile)
   g$gene_name = g$gene_id
   g$transcript_name = g$transcript_id
   export.gff2(g, outfile)
+
 }
 
 # -------------------------------------------------------------------------- #
