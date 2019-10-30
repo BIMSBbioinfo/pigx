d93aea8315f087b328ac36242b596a5f4c496c32
alexg9010 <alex.gos90@gmail.com>
2019-10-30 14:06:03 +0100

    add license header to rules/Align_bwameth_rules.py

--- rules/Align_bwameth_rules.py
+++ rules/Align_bwameth_rules.py
@@ -1,3 +1,26 @@
+# PiGx BSseq Pipeline.
+#
+# Copyright © 2017 Bren Osberg <brendan.osberg@mdc-berlin.de>
+# Copyright © 2017, 2018 Alexander Gosdschan <alexander.gosdschan@mdc-berlin.de>
+# Copyright © 209 Alexander Blume <alexander.blume@mdc-berlin.de>
+# Copyright © 2017 Katarzyna Wreczycka <katwre@gmail.com>
+# Copyright © 2017, 2018 Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
+#
+# This file is part of the PiGx BSseq Pipeline.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ### This file was originally taken and modified from  
 ### https://github.com/katwre/makeWGBSnake/blob/master/Rules/Align_bwameth_rules.py
 
