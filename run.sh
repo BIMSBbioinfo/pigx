a3c62d68f9800201313173ca7ff0112c6f3a9735
borauyar <bora.uyar@mdc-berlin.de>
2020-06-14 19:09:31 +0200

    add a bash script for running the pipeline

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+settings=$1
+nodes=$2
+additional_args=$3
+
+export PYTHONPATH='~/.conda/envs/pigx_crispr/lib/python3.5/site-packages/'
+echo "ARGS: ${additional_args}"
+SRCDIR="." # provide full path to this folder if running in a different directory
+snakefile="${SRCDIR}/snakefile.py"
+snakemake=`which snameke`
+${snakemake}  ${additional_args} -p --configfile ${settings}  --snakefile ${snakefile} -j ${nodes} 
