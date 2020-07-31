ce9735b82e0dca1ca3b19487a3a2af9b7d34307a
borauyar <bora.uyar@mdc-berlin.de>
2020-07-31 09:43:19 +0200

    add licence information

--- src/generateSiteFiles.R
+++ src/generateSiteFiles.R
@@ -1,6 +1,22 @@
-#prepare _site.yml and other Rmd files to be rendered into a html report (see renderSite rule)
+# crispr-DART pipeline
+#
+# Copyright © 2017-2020 Bora Uyar <bora.uyar@mdc-berlin.de>
+#
+# This file is part of the crispr-DART pipeline
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#        "{RSCRIPT} {params.script} {params.report_scripts_dir} {SAMPLE_SHEET_FILE} {CUT_SITES_FILE} {OUTPUT_DIR} {REPORT_DIR} {RSCRIPT} > {log} 2>&1"
 library(data.table)
 library(yaml)
 
--- src/split_bam_by_indels.R
+++ src/split_bam_by_indels.R
@@ -1,3 +1,22 @@
+# crispr-DART pipeline
+#
+# Copyright © 2017-2020 Bora Uyar <bora.uyar@mdc-berlin.de>
+#
+# This file is part of the crispr-DART pipeline
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 library(GenomicAlignments)
 library(Rsamtools)
 library(data.table)
