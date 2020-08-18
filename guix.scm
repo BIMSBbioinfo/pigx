ce35233996ca33e7dbff352fd6d43004054cc419
Ricardo Wurmus <rekado@elephly.net>
2020-08-18 15:52:04 +0200

    Simplify guix.scm.
    Fixes <https://github.com/BIMSBbioinfo/pigx_bsseq/issues/165>.
    
    * guix.scm: Use existing Guix package definition for pigx-bsseq.

--- guix.scm
+++ guix.scm
@@ -1,5 +1,5 @@
 ;;; PiGx_bsseq - reports pipeline for reads from bisulfite experiments.
-;;; Copyright © 2017, 2018 Ricardo Wurmus <rekado@elephly.net>
+;;; Copyright © 2017, 2018, 2020 Ricardo Wurmus <rekado@elephly.net>
 ;;;
 ;;; This file is part of PiGx_bsseq.
 ;;;
@@ -15,96 +15,19 @@
 ;;;  $ guix package --with-source=pigx_bsseq-0.0.1.tar.gz -f guix.scm
 ;;;
 ;;; This environment file was developed for Guix version
-;;; v0.14.0-3177-gbcddf30af
+;;; 1614862
 
-(use-modules (guix packages)
-             (guix licenses)
-             (guix download)
-             (guix build-system gnu)
-             (gnu packages)
-             (gnu packages autotools)
-             (gnu packages base)
-             (gnu packages statistics)
-             (gnu packages bioinformatics)
-             (gnu packages compression)
-             (gnu packages cran)
-             (gnu packages curl)
-             (gnu packages haskell)
-             (gnu packages java)
-             (gnu packages ncurses)
-             (gnu packages perl)
-             (gnu packages pkg-config)
-             (gnu packages python)
-             (gnu packages tls)
-             (gnu packages web))
+(use-modules (guix packages) (gnu packages))
 
 (define %pigx-bsseq-version
   (symbol->string (with-input-from-file "VERSION" read)))
 
-(define-public pigx-bsseq
-  (package
-    (name "pigx_bsseq")
+(define-public pigx-bsseq-development
+  (package (inherit (specification->package "pigx-bsseq"))
     (version %pigx-bsseq-version)
     (source (string-append (getcwd) "/pigx_bsseq-" version ".tar.gz"))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-before 'check 'set-timezone
-           ;; The readr package is picky about timezones.
-           (lambda* (#:key inputs #:allow-other-keys)
-             (setenv "TZ" "UTC+1")
-             (setenv "TZDIR"
-                     (string-append (assoc-ref inputs "tzdata")
-                                    "/share/zoneinfo"))
-             #t))
-         (add-after 'install 'wrap-executable
-           ;; Make sure the executable finds all R modules.
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let ((out (assoc-ref outputs "out")))
-               (wrap-program (string-append out "/bin/pigx-bsseq")
-                 `("R_LIBS_SITE" ":" = (,(getenv "R_LIBS_SITE")))
-                 `("PYTHONPATH"  ":" = (,(getenv "PYTHONPATH")))))
-             #t)))))
     (native-inputs
-     `(("autoconf" ,autoconf)
-       ("automake" ,automake)
-       ("tzdata" ,tzdata)))
-    (inputs
-     `(("coreutils" ,coreutils)
-       ("sed" ,sed)
-       ("grep" ,grep)
-       ("r-minimal" ,r-minimal)
-       ("r-annotationhub" ,r-annotationhub)
-       ("r-dt" ,r-dt)
-       ("r-genomation" ,r-genomation)
-       ("r-methylkit" ,r-methylkit)
-       ("r-rtracklayer" ,r-rtracklayer)
-       ("r-rmarkdown" ,r-rmarkdown)
-       ("r-ggplot2" ,r-ggplot2)
-       ("r-ggbio" ,r-ggbio)
-       ("r-ggrepel" ,r-ggrepel)
-       ("r-reshape2" ,r-reshape2)
-       ("r-matrixstats" ,r-matrixstats)
-       ("r-data-table" ,r-data-table)
-       ("ghc-pandoc" ,ghc-pandoc-1)
-       ("ghc-pandoc-citeproc" ,ghc-pandoc-citeproc-with-pandoc-1)
-       ("python-wrapper" ,python-wrapper)
-       ("python-pyyaml" ,python-pyyaml)
-       ("snakemake" ,snakemake)
-       ("bismark" ,bismark)
-       ("fastqc" ,fastqc)
-       ("multiqc" ,multiqc)
-       ("bowtie" ,bowtie)
-       ("trim-galore" ,trim-galore)
-       ("cutadapt" ,cutadapt)
-       ("samtools" ,samtools)))
-    (home-page "https://github.com/BIMSBbioinfo/pigx_bsseq/")
-    (synopsis "Bisulfite sequencing pipeline from fastq to methylation reports")
-    (description "PiGx BSseq is a data processing pipeline for raw fastq
-read data of bisulfite experiments; it produces reports on aggregate
-methylation and coverage and can be used to produce information on
-differential methylation and segmentation.")
-    (license gpl3+)))
+     `(("autoconf" ,(specification->package "autoconf"))
+       ("automake" ,(specification->package "automake"))))))
 
-pigx-bsseq
+pigx-bsseq-development
