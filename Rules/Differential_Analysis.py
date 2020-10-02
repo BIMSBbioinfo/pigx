960540547d84df69915039fbc01c7bb6d4a5c9c4
Alexander Blume <alex.gos90@gmail.com>
2020-10-02 16:36:56 +0200

    add scripts and rules for counting features

---
+++ Rules/Differential_Analysis.py
@@ -0,0 +1,73 @@
+# ---------------------------------------------------------------------------- #
+def get_differential_analysis_bedfile(wc):
+    analysisDict = config['differential_analysis'][wc.analysis]
+    if ('Peakset' in analysisDict) and ( analysisDict['Peakset']):
+        infiles = os.path.join(PATH_RDS_FEATURE,
+                wc.analysis + '_FeatureCombination.txt'),
+    else:
+        infiles = os.path.join(PATH_PEAK,  
+                wc.analysis, 
+                wc.analysis + "_peaks.narrowPeak" )
+    return(infiles)
+
+def get_differential_analysis_countfiles(wc):
+    analysisDict = config['differential_analysis'][wc.analysis]
+    infiles = expand(
+            os.path.join(
+                PATH_RDS_COUNTS,
+                '{analysis}_{name}_FeatureCounts.rds'), 
+            analysis = wc.analysis, 
+            name = analysisDict['Case'] + analysisDict['Control']
+            )
+    print(infiles)
+    return(infiles)
+
+# ----------------------------------------------------------------------------- #
+rule feature_counting:
+    input:
+        bedfile  = lambda wc: get_differential_analysis_bedfile(wc),
+        bamfile = os.path.join(PATH_MAPPED, GENOME_TYPES['Main'], "{name}", "{name}.sorted.bam")
+    output:
+        outfile  = os.path.join(PATH_RDS_COUNTS,'{analysis}_{name}_FeatureCounts.rds')
+    params:
+        threads   = config['execution']['rules']['feature_counting']['threads'],
+        name = "{analysis}", 
+        params_tool  = PARAMS['feature_counting'],
+        scriptdir = SCRIPT_PATH,
+        Rscript   = SOFTWARE['Rscript']['executable']
+    log:
+        logfile = os.path.join(PATH_LOG, '{analysis}_{name}_feature_counting.log')
+    message:
+        """
+            Running: feature_counting:
+                features:   {input.bedfile}
+                sample:     {input.bamfile}
+                output:     {output.outfile}
+            """
+    run:
+        if (params.name in set(CUSTOM_PARAMS.keys())):
+            if CUSTOM_PARAMS[params.name]:
+                if 'feature_counting' in CUSTOM_PARAMS[params.name]:
+                    params.params_tool.update(CUSTOM_PARAMS[params.name]['feature_counting'])
+        RunRscript(input, output, params, log.logfile, 'Feature_Counting.R')
+
+# ----------------------------------------------------------------------------- #
+rule merge_counts:
+    input:
+        countfiles = lambda wc: get_differential_analysis_countfiles(wc)
+    output:
+        outfile  = os.path.join(PATH_REPORTS,'{analysis}','{analysis}_FeatureCounts.tsv'),
+        statfile  = os.path.join(PATH_REPORTS,'{analysis}','{analysis}_FeatureCounts_Stats.tsv')
+    params:
+        scriptdir = SCRIPT_PATH,
+        Rscript   = SOFTWARE['Rscript']['executable']
+    log:
+        logfile = os.path.join(PATH_LOG, '{analysis}_merge_counts.log')
+    message:
+        """
+            Running: merge_counts:
+                features:   {input}
+                output:     {output.outfile}
+            """
+    run:
+        RunRscript(input, output, params, log.logfile, 'Merge_Counts.R')
---
+++ scripts/Feature_Counting.R
@@ -0,0 +1,98 @@
+# ---------------------------------------------------------------------------- #
+options = commandArgs(trailingOnly=TRUE)
+source(file.path(options[2],'/Argument_Parser.R'))
+argv = Parse_Arguments('runFeatureCounts')
+
+# ---------------------------------------------------------------------------- #
+## Count reads using Rsubread
+runFeatureCounts <- function(peaks,
+                             bam_files,
+                             outRDS,
+                             tempDir = tempdir("featureCounts_temp"),
+                             ..., 
+                             blacklisted = NULL,
+                             n.cores = 1
+                             ) {
+
+    suppressPackageStartupMessages({
+        require(Rsubread)
+        require(GenomicRanges)
+        require(rtracklayer)
+    })
+
+    if(!all(file.exists(bam_files))){
+        stop(ipaste("Given bam files do not exist.","\n\t",
+                    head(bam_files[!file.exists(bam_files)])
+                    ))
+    }
+
+    fc_args <- list(...)
+    
+    message(paste("Started at ",timestamp(quiet = TRUE)))
+
+    ## load peaks if the argument is a file
+    if(is.character(peaks)) {
+        if(!file.exists(peaks)){
+            stop(paste("Given peak file do not exist.","\n\t",peaks))
+        }
+        if(grepl("txt$",peaks)) {
+            peaks <- read.delim(peaks)
+            names(peaks) <- c("chr","start","end", "name","class")
+            peaks <- GenomicRanges::makeGRangesFromDataFrame(peaks,
+            keep.extra.columns = TRUE)
+        } else {
+            peaks <- rtracklayer::import(peaks)
+        }
+    }
+
+    ## load blackliste regions
+    if (!is.null(blacklisted)) {
+        if(is.character(blacklisted)) {
+            blacklisted <- rtracklayer::import.bed(blacklisted)
+            peaks <- peaks[!peaks %over% blacklisted]
+        }
+    }
+
+
+    if(!"name" %in% names(mcols(peaks)))  {
+        peaks$name <- paste0("cons_peak_",seq_len(nrow(peaks)))
+    }
+    peaks_saf <- as.data.frame(peaks)[c("name","seqnames","start","end","strand")]
+    names(peaks_saf) <- c("GeneID","Chr","Start","End","Strand")
+
+    message("Counting ...")
+    message(timestamp(quiet = TRUE))
+            
+    if(!dir.exists(tempDir)) dir.create(tempDir)    
+
+    ## start counting
+    fc <- Rsubread::featureCounts(bam_files,
+                                  annot.ext = peaks_saf,
+                                  ...,
+                                  tmpDir = tempDir,
+                                  nthreads = n.cores)
+    fc$targets <- basename(bam_files)
+    saveRDS(fc,outRDS) 
+
+    message("Counting done.")
+    message(timestamp(quiet = TRUE))
+
+    message(paste("Finished at ",timestamp(quiet = TRUE)))
+}
+
+# ---------------------------------------------------------------------------- #
+# function call
+
+## debug
+# print(argv)
+## END debug
+
+runFeatureCounts(
+                 peaks = argv$input[['bedfile']],
+                 bam_files =  argv$input[['bamfile']],
+                 outRDS = argv$output[["outfile"]],
+                 tempDir = dirname(argv$output[["outfile"]]),
+                 blacklisted = NULL,
+                 n.cores = argv$params[["threads"]],
+                 argv$params[["params_tool"]]
+)
