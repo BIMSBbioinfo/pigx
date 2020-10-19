6254da40fce6e6a5b3bce20e966740b744c03739
Alexander Blume <alex.gos90@gmail.com>
2020-10-19 11:17:14 +0200

    add copyright header for differential_analysis rules

--- Rules/Differential_Analysis.py
+++ Rules/Differential_Analysis.py
@@ -1,3 +1,25 @@
+# PiGx ChIPseq Pipeline.
+#
+# Copyright © 2020 Alexander Blume <alexander.blume@mdc-berlin.de>
+#
+# This file is part of the PiGx ChIPseq Pipeline.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Set of Rules to prepare input for differential analysis report.
+
+
 # ---------------------------------------------------------------------------- #
 def get_differential_analysis_bedfile(wc):
     analysisDict = config['differential_analysis'][wc.analysis]
@@ -26,7 +48,6 @@ def get_differential_analysis_countfiles(wc):
             name = samps
 
             )
-    print(infiles)
     return(infiles)
 
 def get_differential_analysis_settings(wc, setting):
