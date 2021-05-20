608a28707d429c52f9deb6883a2510597b21743f
Ricardo Wurmus <rekado@elephly.net>
2021-05-20 23:53:27 +0200

    parse_vep: Simplify.

--- scripts/parse_vep.py
+++ scripts/parse_vep.py
@@ -4,27 +4,19 @@ import csv
 import sys
 
 def parse_vep(vep_dir, input, output):
-    header_line = None
-
     with open(input, 'r') as file:
-        with open(os.path.join(vep_dir, "tmp.txt"), 'w+') as clean_file:
-            for num, line in enumerate(file, 1):
-                if re.match("#Uploaded_variation", line):
-                    header_line = num
-                    clean_file.write(line[1:])
-                    break
-            for num, line in enumerate(file, header_line):
-                clean_file.write(line)
-
-    with open(os.path.join(vep_dir,'tmp.txt'), 'r') as file:
         with open(output, 'w') as output:
+            header = None
+            for line in file:
+                if line.startswith("#Uploaded_variation"):
+                    header = line[1:].strip().split("\t")
+                    break
+
             reader = csv.reader(file, delimiter="\t")
             writer = csv.writer(output, lineterminator='\n')
 
             # make new column SYMBOL for gene names 
-            # fixme: This is redundant with the making of the header line above, but works for now
             gene_column = []
-            header = next(reader)
             header.append("SYMBOL")
             gene_column.append(header)
 
@@ -38,7 +30,6 @@ def parse_vep(vep_dir, input, output):
                         gene_column.append(row)
 
             writer.writerows(gene_column)
-    # TODO: add step to clean "tmp.txt" 
 
 if __name__ == '__main__':
     vep_dir = sys.argv[1]
