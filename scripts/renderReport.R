39de080014adeefa6a6a78a63eb0ce25b9b4cbc2
Ricardo Wurmus <rekado@elephly.net>
2021-05-28 00:48:59 +0200

    renderReport: Add CSS.

--- scripts/renderReport.R
+++ scripts/renderReport.R
@@ -9,7 +9,23 @@ header <- args[3]
 parameters <- fromJSON (args[4])
 
 css <- tempfile (fileext="css")
-cat ("body { padding-top: 60px }", file = css)
+cat ("\
+body { font-size: 20px; padding-top: 60px; position: absolute; } \
+span.header-section-number { \
+  color: #93a1a1; \
+  position: absolute; \
+  text-align: right; \
+  width: 2em; \
+  margin-left: -2.5em } \
+div#TOC { font-size: smaller } \
+div#TOC span.header-section-number { display: none } \
+p { line-height: 1.5 } \
+h1 { margin-top: 2em } \
+h1.title { margin-top: 20px } \
+h2 { margin-top: 2em } \
+h4.date { color: #93a1a1 } \
+h4.date:before { content: \"Created: \" } \
+", file = css)
 
 settings <- html_document(includes=includes(before_body=header),
                           css=css,
