9154eb3b7bf6a98b740eb1afa9428d6bae8229fc
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-11 15:12:34 +0200

    snakefile: remove outdated todo

--- snakefile.py
+++ snakefile.py
@@ -209,8 +209,6 @@ rule samtools_filter_aligned:
     log: os.path.join(LOG_DIR, 'samtools_filter_aligned_{sample}.log')
     shell: "{SAMTOOLS_EXEC} view -bh -f 2 -F 2048 {input} > {output} 2>> {log} 3>&2"
 
-
-# TODO: check command to get unaligned ones
 rule samtools_filter_unaligned:
     input: os.path.join(MAPPED_READS_DIR, '{sample}_aligned_tmp.sam')
     output: os.path.join(MAPPED_READS_DIR, '{sample}_unaligned.bam')
