3690e182da62b8b86d757897792ed4d350c1cb6d
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-13 10:26:57 +0200

    snakefile: adding prinseq_se

--- snakefile.py
+++ snakefile.py
@@ -211,6 +211,7 @@ rule bwa_index:
     log: os.path.join(LOG_DIR, 'bwa_index.log')
     shell: """mkdir -p {INDEX_DIR};
 ln -sf {input} {INDEX_DIR};
+cd {INDEX_DIR};
 {BWA_EXEC} index {output.ref} >> {log} 2>&1"""
 
 
