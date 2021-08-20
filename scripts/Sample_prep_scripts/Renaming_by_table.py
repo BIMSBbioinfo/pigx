13ef0f28b5926be51a765ffec6d44dbcbdb94de2
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-20 20:55:16 +0200

    add script for renaming and merging process

---
+++ scripts/Sample_prep_scripts/Renaming_by_table.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+import sys
+
+def renaming(dir, bash_script, naming_table):
+    os.chdir(dir)
+    
+    
+
+def merging():
+    pass
+
+def sorting():
+    pass 
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
