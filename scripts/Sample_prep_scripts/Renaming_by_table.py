3fa8feb3e3ddbb22c2977d9c30a1aaec79dc4473
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-21 00:18:59 +0200

    typos

--- scripts/Sample_prep_scripts/Renaming_by_table.py
+++ scripts/Sample_prep_scripts/Renaming_by_table.py
@@ -1,27 +1,38 @@
 import os
-import argparse
 import sys
 import subprocess
 
-def renaming(dir, bash_script, naming_table):
-    os.chdir(dir)
-    res = subprocess.call(bash_script)
 
-def merging(dir):
-    if os.path.isdir(dir):
-        for file1 in os.listdir(dir):
-            for file2 in os.listdir(dir):
-                if file1 == file2:
+def renaming(sample_dir, bash_script, naming_table):
+    os.chdir(sample_dir)
+    print("Entering ", sample_dir)
+    res = subprocess.call([bash_script, naming_table])
+    print(res)
+
+
+def merging(sample_dir):
+    if os.path.isdir(sample_dir):
+        for i in os.listdir(sample_dir):
+            file1 = i.split("_R")[0]
+            for j in os.listdir(sample_dir):
+                file2 = i.split("_R")[0]
+                if file1 + "_R1.fastq.gz" == file2:
                     new = file1.split(".", 2)[0] + "_merged.fastq.gz"
-                    zcat = subprocess.call("zcat ", file1, " ", file2, "> ", new)
+                    zcat_command = f'zcat {file1} {file2} > {new}'
+                    res = subprocess.call(zcat_command)
+                    print(zcat)
     else:
         raise Exception("directory not found")
 
-def sorting():
-    pass 
 
 def main():
-    pass
+    sample_dir = sys.argv[0]
+    bash_script = sys.argv[1]
+    naming_table = sys.argv[2]
+
+    renaming(dir, bash_script, naming_table)
+    merging(dir)
+
 
 if __name__ == '__main__':
     main()
