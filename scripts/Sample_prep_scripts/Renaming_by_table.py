32907176f3256df345d555c40356ff94538cf0c7
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-20 21:25:19 +0200

    renaming.py: adding renaming and merging function

--- scripts/Sample_prep_scripts/Renaming_by_table.py
+++ scripts/Sample_prep_scripts/Renaming_by_table.py
@@ -1,14 +1,21 @@
 import os
 import argparse
 import sys
+import subprocess
 
 def renaming(dir, bash_script, naming_table):
     os.chdir(dir)
-    
-    
+    res = subprocess.call(bash_script)
 
-def merging():
-    pass
+def merging(dir):
+    if os.path.isdir(dir):
+        for file1 in os.listdir(dir):
+            for file2 in os.listdir(dir):
+                if file1 == file2:
+                    new = file1.split(".", 2)[0] + "_merged.fastq.gz"
+                    zcat = subprocess.call("zcat ", file1, " ", file2, "> ", new)
+    else:
+        raise Exception("directory not found")
 
 def sorting():
     pass 
