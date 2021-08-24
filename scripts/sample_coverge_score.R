a778c47f4800c7638c5874726e3e2ceb30a40279
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-24 23:35:02 +0200

    add Rscript to get coverage values

---
+++ scripts/sample_coverge_score.R
@@ -0,0 +1,21 @@
+get_genome_cov <- function ( coverage_dir ) {
+  # TODO check assumptions e.g. if the coverage files exists in dir and if the dir exists
+  require(dplyr)
+  
+  files <- list.files(path = coverage_dir,
+                      pattern = "_coverage.csv",
+                      full.names = TRUE, 
+                      recursive = FALSE) # why false?
+  genome_cov.df <- data.frame(samplename = c(),
+                              coverage = c())
+  genome_cov.df <- bind_rows(genome_cov.df, lapply( files, function (x){
+    rd_tbl <- read.table(x, sep = '\t') # ignores the row starting with # directly TODO: how to get column names?
+    # TODO check if file has the correct header format
+    data <- c( samplename = strsplit( basename (x), "\\.csv" )[[1]],
+               coverage = rd_tbl$V6 )
+    #df <- bind_rows(df, data)
+    return(data)
+  }))
+  
+  return(genome_cov.df) 
+}
