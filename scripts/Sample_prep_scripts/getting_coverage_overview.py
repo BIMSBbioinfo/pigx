f2fb8a6abf2257b67b2d9f9fe71fd690217ae65c
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-24 09:56:39 +0200

    adding script and output for getting an overview about all coverage values

---
+++ scripts/Sample_prep_scripts/getting_coverage_overview.py
@@ -0,0 +1,27 @@
+import os
+import csv 
+
+def make_table_with_cov( coverage_dir, output_file ):
+    for file in os.listdir(coverage_dir):
+        coverage = 0
+        if "_coverage.csv" in file:
+            samplename = file.split("_cov")[0]
+            
+            with open(os.path.join(coverage_dir, file), "r") as cov_csv:
+                rdr = csv.reader(cov_csv, delimiter = "\t")
+                for row in rdr: 
+                    if not "#" in row[0] and "NC" in row[0]:
+                        coverage = row[5]
+            
+            fields = [samplename, coverage]
+            
+            with open( output_file, "a") as output: 
+                wrtr = csv.writer(output)
+                wrtr.writerow(fields)
+                
+#coverage_dir = "/mnt/beast/pathogenomics/pigx_ww/210822_ww_berlin_febjune_publication/output/coverage"
+coverage_dir = "/mnt/bimsb_local/pigx_sarscov2_ww/210531_wastewaterARTICEW109_pigxsarscov2ww_default/output/coverage"
+#output_file = "/home/vfs/PycharmProjects/Akalinlab_pathogenomics/pigx_sarscov2_ww/tests/coverage_gathering.csv"
+output_file = "/home/vfs/PycharmProjects/Akalinlab_pathogenomics/pigx_sarscov2_ww/tests/coverage_gathering_oldARTIC_analysis.csv"
+
+make_table_with_cov(coverage_dir, output_file)
