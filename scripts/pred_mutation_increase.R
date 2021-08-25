6e3bde789a64f7e399a1fb06c74b71c62dfacd9e
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-25 04:21:41 +0200

    mutation_increase: transform into functions

--- scripts/pred_mutation_increase.R
+++ scripts/pred_mutation_increase.R
@@ -1,57 +1,67 @@
-library(tidyverse)
 
-# -----------TEST---------------
-mutation_freq <- read_delim( "/mnt/beast/pathogenomics/vpipe/akalinlab_pathogenomics/misc_src/pigx_sarscov2_ww/tests/output/variants/data_mutation_plot.csv", "\t", 
+parsing_mutation_plot_data <- function ( mutation_plot_data ){
+  #' taking csv as input which will be made by the variant reports
+  #' returns df containing only mutations with frequency values
+  
+  require(tidyverse)
+  # TODO check file format assumptions
+  
+  # open the table
+  mutation_freq <- read_delim( mutation_plot_data, "\t", 
                              escape_double = FALSE, trim_ws = TRUE )
-# -----------TEST---------------
-
-#open the table
-# TODO file input
-# mutation_freq <- read_delim( "C:/Users/rafae/Desktop/mutation_freq.csv", "\t", escape_double = FALSE, trim_ws = TRUE )
-
-#  get names of mutations (everything after the meta info from the sample sheet)
-mutations <- names ( 
+  
+  #  get names of mutations (everything after the meta info from the sample sheet)
+  mutations <- names ( 
             mutation_freq[ (which( names(mutation_freq) %in% "coordinates_long")+1) : length( names( mutation_freq ))])
-
-#enforcing date type for column dates ...it will get rid of the time
-mutation_freq$dates <- as.Date ( mutation_freq$dates )
-
-#remove mutations with NA for all rows and create a new dataframe
-# TODO what is x?
-not_all_na <- function( x ) any( !is.na( x ) )
-lm_df <- mutation_freq %>% select( c( dates, all_of(mutations) )) %>% select( where( not_all_na ))
-
-#get a list of mutations that still present after removing those with only NA values
-mutations_with_values <- names( lm_df[ 2 : length( names( lm_df ))])
-
-#initialize 3 lists for results
-results_lm <- list ()
-summaries <- list ()
-pvalues <- list ()
-
-#loop the mutations, doing the linear model, and extracting the pvalues
-for ( i in mutations_with_values ){
-  tmp <- lm_df %>% select( dates, all_of(i) )
-  test <- lm( formula = tmp[[i]] ~ tmp$dates )
-  results_lm[[i]] <- test
-  summaries[[i]] <- summary( test )
-  pvalues[[i]] <- as.data.frame( summary( test )$coefficients[,4]) 
-  colnames( pvalues[[i]] ) <- "pvalues"
+  
+  # enforcing date type for column dates ...it will get rid of the time
+  mutation_freq$dates <- as.Date ( mutation_freq$dates )
+  
+  # remove mutations with NA for all rows and create a new dataframe
+  # fixme Vic I think there is a more compact way for this
+  not_all_na <- function( x ) any( !is.na( x ) )
+  lm_df <- mutation_freq %>% select( c( dates, all_of(mutations) )) %>% select( where( not_all_na ))
+  
+  return ( lm_df )
 }
 
-#generate a proper dataframe with pvalues, filtering by significance
-pvalues_df <- do.call( rbind, pvalues ) %>% 
-              tibble::rownames_to_column( "VALUE" ) %>% 
-              filter( str_detect( VALUE, "dates" ) ) %>% 
-              filter( pvalues < 0.05 ) 
 
-#names of mutations get strange pattern, doing some split and getting the names correct
-pvalues_df$mutation <- str_split_fixed( pvalues_df$VALUE, "[.]",2 )[,1] # TODO this assumes values
-
-#making a list with mutations that are significant
-mutations_sig <- str_split_fixed( pvalues_df$VALUE, "[.]", 2 )[,1]
-
-#fixing the dataframe with mutations and pvalues
-pvalues_df <- pvalues_df %>% select( mutation, pvalues )
-
-# TODO put this in a set of functions you call then...probably from the index report.
+lin_reg_mutation_change <- function( mutations.df ){
+  #' takes data frames with mutations, frequency values over time
+  #' returns A) list with mutations with significant change, B) dataframe with related pvalues
+  
+  require(tidyverse)
+  # TODO check file format assumptions
+  
+  #initialize 3 lists for results
+  results_lm <- list ()
+  summaries <- list ()
+  pvalues <- list ()
+  
+  #loop the mutations, doing the linear model, and extracting the pvalues
+  for ( i in names(mutations.df[,-which(names(mutations.df) %in% "dates")]) ){
+    tmp <- mutations.df %>% select( dates, all_of(i) )
+    test <- lm( formula = tmp[[i]] ~ tmp$dates )
+    results_lm[[i]] <- test
+    summaries[[i]] <- summary( test )
+    pvalues[[i]] <- as.data.frame( summary( test )$coefficients[,4]) 
+    colnames( pvalues[[i]] ) <- "pvalues"
+  }
+  
+  #generate a proper dataframe with pvalues, filtering by significance
+  pvalues_df <- do.call( rbind, pvalues ) %>% 
+                tibble::rownames_to_column( "VALUE" ) %>% 
+                filter( str_detect( VALUE, "dates" ) ) %>% 
+                filter( pvalues < 0.05 ) 
+  
+  #names of mutations get strange pattern, doing some split and getting the names correct
+  pvalues_df$mutation <- str_split_fixed( pvalues_df$VALUE, "[.]",2 )[,1] 
+  
+  #making a list with mutations that are significant
+  mutations_sig <- str_split_fixed( pvalues_df$VALUE, "[.]", 2 )[,1]
+  
+  #fixing the dataframe with mutations and pvalues
+  pvalues_df <- pvalues_df %>% select( mutation, pvalues )
+  
+  return ( c(mutations_sig, pvalues_df))
+}
