1a41637299e94120d3662675cbb9497b05921a0f
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-08-25 13:29:18 +0200

    snakefile: remove bbduk and realted rules since it gave not the result we aimed for

--- snakefile.py
+++ snakefile.py
@@ -86,8 +86,6 @@ PYTHON_EXEC          = tool("python")
 RSCRIPT_EXEC         = tool("Rscript")
 SAMTOOLS_EXEC        = tool("samtools")
 VEP_EXEC             = tool("vep")
-BBDUK_EXECT          = tool("bbduk")
-BEDTOOLS_EXEC        = tool("bedtools")
 
 ## Load sample sheet
 with open(SAMPLE_SHEET_CSV, 'r') as fp:
@@ -240,42 +238,6 @@ rule prinseq_pe:
             {GZIP_EXEC} {params.tmp_r1} && mv {params.tmp_r1}.gz {output.r1} && \
             {GZIP_EXEC} {params.tmp_r2} && mv {params.tmp_r2}.gz {output.r2}")
 
-# TODO we have to check with some benchmark samples if - when we do only quality trimming the bbduk - if the alignment results are similar to when we do it with prinseq
-# TODO create variables 
-# parameters taken from here: https://github.com/AAFC-BICoE/snakemake-barcoding-assembly-pipeline/blob/3798585eb78b948116323b1f787e98bc141b518c/Snakefile#L33
-
-# this is just an intermediate version to get our results, it has to be reworked to fit whatever input you'd like it to get
-rule bbduk_adapter_trimming: 
-    input:
-        bba_r1 = os.path.join(TRIMMED_READS_DIR, "{sample}_trimmed_R1.fastq.gz"),
-        bba_r2 = os.path.join(TRIMMED_READS_DIR, "{sample}_trimmed_R2.fastq.gz")
-    output:
-        out1 = os.path.join(TRIMMED_READS_DIR, "{sample}_adapter-trimmed_R1.fastq.gz"),
-        out2 = os.path.join(TRIMMED_READS_DIR, "{sample}_adapter-trimmed_R2.fastq.gz")
-    params: 
-        adapt_seq = "AGATCGGAAGAG" # should be "Illumina univeral adapter" after: https://www.biostars.org/p/371399/, but there is also a TrustSeq adapter in the fasttwc - maybe I have to ask Emanuel for it, 
-    log: os.path.join(LOG_DIR, 'bbduk_adapter_{sample}.log')
-    shell: """
-            {BBDUK_EXECT} in1={input.bba_r1} out1={output.out1} in2={input.bba_r2} out2={output.out2}\
-            literal={params.adapt_seq} qtrim=rl trimq=10 ktrim=r k=23 mink=11 hdist=1 tpe tbo &>{log}
-           """
-    
-# TODO make se version, later input and output should be given dynamically 
-# altough the shell command is the same then with bbduk_adapter_trimming I would keep it seperate in case one of the steps isn't wanted or needed
-rule bbduk_primer_trimming: 
-    input:
-        bbp_r1 = os.path.join(TRIMMED_READS_DIR, "{sample}_adapter-trimmed_R1.fastq.gz"),
-        bbp_r2 = os.path.join(TRIMMED_READS_DIR, "{sample}_adapter-trimmed_R2.fastq.gz"), 
-        primers = os.path.join(INDEX_DIR, "primer_sequences.fa")
-    output:
-        bbp_out1 = os.path.join(TRIMMED_READS_DIR, "{sample}_primer-trimmed_R1.fastq.gz"),
-        bbp_out2 = os.path.join(TRIMMED_READS_DIR, "{sample}_primer-trimmed_R2.fastq.gz")
-    log: os.path.join(LOG_DIR, 'bbduk_primer_{sample}.log')
-    shell:  """
-            {BBDUK_EXECT} in1={input.bbp_r1} out1={output.bbp_out1} in2={input.bbp_r2} out2={output.bbp_out2}\
-            ref={input.primers} qtrim=rl trimq=10 ktrim=r k=23 mink=11 hdist=1 tpe tbo &>{log};
-           """
-
 rule bwa_index:
     input: REFERENCE_FASTA
     output:
@@ -292,7 +254,7 @@ rule bwa_index:
 # TODO: use map_input as input 
 rule bwa_align:
     input:
-        fastq = [os.path.join(TRIMMED_READS_DIR, "{sample}_primer-trimmed_R1.fastq.gz"), os.path.join(TRIMMED_READS_DIR, "{sample}_primer-trimmed_R2.fastq.gz")],
+        fastq = [os.path.join(TRIMMED_READS_DIR, "{sample}_trimmed_R1.fastq.gz"), os.path.join(TRIMMED_READS_DIR, "{sample}_trimmed_R2.fastq.gz")],
         ref = os.path.join(INDEX_DIR, "{}".format(os.path.basename(REFERENCE_FASTA))),
         index = os.path.join(INDEX_DIR, "{}.bwt".format(os.path.basename(REFERENCE_FASTA)))
     output: os.path.join(MAPPED_READS_DIR, '{sample}_aligned_tmp.sam')
