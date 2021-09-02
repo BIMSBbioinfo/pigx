6660cc11c006c5684032c4fec9b3ed16167101e7
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-09-02 16:53:34 +0200

    smpl_cov_scr.R: add function for summing amplicon coverage values

--- scripts/sample_coverage_score.R
+++ scripts/sample_coverage_score.R
@@ -19,3 +19,26 @@ get_genome_cov <- function ( coverage_dir ) {
   
   return(genome_cov.df) 
 }
+
+get_amplicon_cov <- function ( coverage_dir ) {
+  # TODO check assumptions e.g. if the coverage files exists in dir and if the dir exists
+  require(dplyr)
+  
+  files <- list.files(path = coverage_dir,
+                      pattern = "_merged_covs.csv",
+                      full.names = TRUE, 
+                      recursive = FALSE) # why false?
+  amplicon_cov.df <- data.frame(samplename = c(),
+                              total_amps_cvrd = c(),
+                              drop_out_amps = c())
+  amplicon_cov.df <- bind_rows(amplicon_cov.df, lapply( files, function (x){
+    rd_tbl <- read.table(x, sep = '\t', header = TRUE) 
+    # TODO check if file has the correct header format
+    data <- c( samplename = strsplit( basename (x), "\\_coverage.csv" )[[1]],
+               amplicon_cov = rd_tbl$Total.number.of.amplicons.fully.covered,
+               drop_out_amps = rd_tbl$Drop.out.amplicons)
+    return(data)
+  }))
+  
+  return(amplicon_cov.df) 
+}
