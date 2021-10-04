fcbacf44b94d2d348aa8b2824bcf6f1f986d4b8b
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-10-04 15:41:14 +0200

    rlm: handling of no pvalues

--- scripts/pred_mutation_increase.R
+++ scripts/pred_mutation_increase.R
@@ -96,14 +96,19 @@ robust_reg_mutation_change <- function( mutations.df ){
       }
       }
   }
+  if ( length(coeff) > 0){
   coeff_df <- as.data.frame(do.call(rbind, coeff)) %>% tidyr::drop_na() %>%
           tibble::rownames_to_column( "VALUE" )
+  } else { coeff_df <- data.frame()}
+  if ( length(pvalues) > 0) {
   pvalues.df <- data.frame(do.call(rbind, pvalues)) %>%
                 tibble::rownames_to_column( "VALUE" ) %>%
                 left_join(coeff_df, by = "VALUE")
-  
+
   colnames(pvalues.df) <- c("mutation", "value", "coefficient")
   pvalues.df <- pvalues.df %>% filter(value < 0.05)
+  } else {pvalues.df <- data.frame() }
+
   
   return(pvalues.df)
 }
