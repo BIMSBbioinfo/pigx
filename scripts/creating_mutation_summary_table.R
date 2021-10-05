1cb1faa29c63b70cfe91bc539bdd1a7065185f8d
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-10-05 12:40:37 +0200

    add file to create data_mutations_plot from folder of single files per sample

---
+++ scripts/creating_mutation_summary_table.R
@@ -0,0 +1,49 @@
+
+library(dplyr)
+library(stringr)
+
+get_files <- function ( mutations_dir){
+  files <- list.files(path = mutations_dir,
+                    pattern = "_mutations.csv",
+                    full.names = TRUE,
+                    recursive = FALSE)
+  return (files)
+}
+
+create_summary <- function ( files, output_file ){
+
+  require(dplyr)
+
+  cat( paste("Summarizing", length(files), "mutation files.", collapse = "\s") )
+
+  i <- 0
+  while ( i != length(files)){
+
+    if ( file.exists (output_file) ) {
+      df1 <- read.table( output_file,
+                         sep = "\t", header = TRUE, colClasses = "character", check.names = FALSE )
+    } else {
+      df1 <- read.csv(files[1], header = TRUE, colClasses = "character", check.names = FALSE)
+        # write to output file
+      write.table(df1, output_file, sep = "\t",
+          row.names = FALSE, quote = FALSE)
+      i <- i + 1
+    }
+
+    i <- i + 1
+    df2 <- read.csv(files[i], header = TRUE, colClasses = "character", check.names = FALSE)
+    output_df <- dplyr::full_join(df1, df2, by = intersect(colnames(df1),
+                                                    colnames(df2)), copy = TRUE)
+    # write to output file
+    write.table(output_df, output_file, sep = "\t",
+          row.names = FALSE, quote = FALSE)
+  }
+}
+
+args <- commandArgs (trailingOnly=TRUE)
+mutations_dir <- args[1]
+output_file <- args[2]
+
+files <- get_files( mutations_dir )
+create_summary( files, output_file )
+
