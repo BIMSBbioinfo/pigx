fe2793bc1104a91a9f8eb25befef14a8491fa05b
Vic-Fabienne Schumann <vfschumann@icloud.com>
2021-10-19 16:05:27 +0200

    pred_mut_increase: adding refined lm model and pvalue filtering
    refined_lm_model succeeds "gather_lm_values" and generates lm results for all values
    
     filter_lm_res_top20 succeeds "lin_reg_mutation_change" and applies the filter steps for siginificance on the full-lm-results as provided by refined_lm_model

--- scripts/pred_mutation_increase.R
+++ scripts/pred_mutation_increase.R
@@ -65,7 +65,6 @@ lin_reg_mutation_change <- function( mutations.df ){
 }
 
 
-
 gather_lm_values <- function(mutations.df){
  #' takes data frames with mutations, frequency values over time
   #' returns dataframe with related pvalues
@@ -110,4 +109,78 @@ gather_lm_values <- function(mutations.df){
                                        `test[\"coefficients\"]$coefficients[\"tmp$dates\"]` )
   colnames(pvalues_df) <- c("mutation", "pvalues", "coefficients")
   return ( pvalues_df)
+}
+
+refined_lm_model <- function( mutations.df ){
+ #' takes data frames with mutations, frequency values over time
+ #' returns dataframe with related pvalues
+  
+  require(tidyverse)
+  require(stringr)
+ 
+  # TODO check file format assumptions
+  
+  # for mutation frequency - a missing value can be assumed as "not found", so NA can be set to 0
+  mutations.df <- mutations.df %>% replace(is.na(.), 0)
+  
+  #initialize 3 lists for results
+  results_lm <- list ()
+  summaries <- list ()
+  pvalues <- list () 
+  coeff <- list()
+  
+  #loop the mutations, doing the linear model, and extracting the pvalues
+  for ( i in names( mutations.df[,-which(names(mutations.df) %in% "dates")]) ){
+    # take only mutations that were found in at least 5 samples (equals 5 values above 0) to ensure robust model calculation
+    if ( nrow( filter( tmp, values > 0 ) ) >= 5 ){
+      # only select mutation columns
+      tmp <- mutations.df %>% select( dates, all_of(i) )
+      # perform linear regression
+      test <- lm( formula = tmp[[i]] ~ tmp$dates )
+      # extract results from model
+      coeff[[i]] <- as.data.frame(test["coefficients"]$coefficients["tmp$dates"])
+      results_lm[[i]] <- test
+      summaries[[i]] <- summary( test )
+      pvalues[[i]] <- as.data.frame( summary( test )$coefficients[,4])
+      }
+    }
+  
+  # generate results data frame with all values
+  coeff_df <- as.data.frame(do.call(rbind, coeff)) %>% tidyr::drop_na() %>%
+              # generate a proper dataframe from coeff values
+              tibble::rownames_to_column( "VALUE" ) %>% 
+              # proper column names
+              rename(mutation = VALUE, coefficients = "test[\"coefficients\"]$coefficients[\"tmp$dates\"]" )
+  
+  lm_res.df <-  do.call( rbind, pvalues ) %>%
+                # make proper df from p-values
+                tibble::rownames_to_column( "VALUE" ) %>% 
+                # only take the actual p-value
+                filter( stringr::str_detect( VALUE, "dates" ) ) %>%
+                # split the suffix from the model away
+                mutate(VALUE = str_split(VALUE, ".tmp", simplify = TRUE)[,1]) %>%
+                # proper column names
+                rename(mutation = VALUE, pvalues = "summary(test)$coefficients[, 4]" ) %>%
+                # join coeffs and pvalues togehter
+                left_join(coeff_df, by = "mutation")
+  
+  return ( lm_res.df )
+}
+
+filter_lm_res_top20 <- function ( lm_res.df, pvalue_cutoff){
+  #' lm_res.df: a dataframe with variables, pvalues and regression coefficients 
+  #' pvalue_cutoff: a numeric value to use as p-value cutoff for filtering
+  
+  # generate dataframe with significant results only
+  lm_res_sig.df <- lm_res.df %>% 
+                    # filter for increasing trends only
+                    filter( coefficients > 0) %>%
+                    # filter for significance
+                    filter( pvalues < pvalue_cutoff) %>% 
+                    # sort for decreasing coeffs
+                    arrange( desc(coefficients)) %>% 
+                    # only take the 20 strongest trends
+                    slice_head(n = 20)
+  
+  return ( lm_res_sig.df )
 }
