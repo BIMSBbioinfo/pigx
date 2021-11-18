a1dc22126fa193dba78a93527b168f0a6a02821f
Alexander Blume <alex.gos90@gmail.com>
2021-11-18 15:23:29 +0100

    turn tempdir into prefix

--- rules/deduplicate_samblaster.py
+++ rules/deduplicate_samblaster.py
@@ -31,15 +31,15 @@ rule samblaster_markdup_sort:
     params:
         threads=config['execution']['rules']['samblaster_markdup_sort']['threads'],
         memory=config['execution']['rules']['samblaster_markdup_sort']['memory'],
-        tmpdir=DIR_sorted+"{sample}/"
+        prefix=DIR_sorted+"{sample}"
     log:
         DIR_sorted+"{sample}_markdups.log"
     message: fmt("Deduplicating reads with samblaster for sample {{sample}}")
     shell:
         nice("samtools", 
         ["view -h {input}"," | ", 
-        tool("samblaster"),toolArgs("samblaster"),"2> {log}","|",
-        tool("samtools"),"sort","-T={params.tmpdir}",
+        tool("samblaster"),toolArgs("samblaster"),"2>> {log}","|",
+        tool("samtools"),"sort","-T={params.prefix}",
          "-o {output.bam}", "-@ {params.threads}", 
          "-m {params.memory}", "-l 9","2> {log}",";",
          tool("samtools"),"index {output.bam}"],("{log}"))
